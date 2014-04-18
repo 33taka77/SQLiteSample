7ed4b7c51da36ef88d907077f9ee737e6d8c9539
Aizawa Takashi <aizawatakashi@Aizawa-no-MacBook-Pro.local>
2014-04-18 16:15:32 +0900

    Initial Commit

---
+++ SQLiteSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SQLiteSample
+//
+//  Created by Aizawa Takashi on 2014/04/18.
+//  Copyright (c) 2014年 Aizawa Takashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
